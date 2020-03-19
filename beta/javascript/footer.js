1a6db296c7f905668954f4930d0a8ea82633b5b4
yllekz <nothingnothingnothin@icloud.com>
2020-03-19 11:18:41 -0400

    CSS and content things
    Cleaned up CSS errors, removed legacy IE support in spots
    
    Adjusted links in index
    
    Updated footer content

--- beta/javascript/footer.js
+++ beta/javascript/footer.js
@@ -1,8 +1,6 @@
 document.write('<span class="jsfooter">');
 
-document.write('<p>Hope This Helps</p>');
-
-document.write('<p><a href="https://twitter.com/HTHTHEPODCAST">Twitter</a></p>');
+document.write('<p>Hope This Helps. Copyright 2020</p>');
 
 //document.write('<p><a href="http://unhmub.com/off_campus/" target="_blank">Commuter Student Services</a>&nbsp;|&nbsp;<a href="http://unhmub.com/insidetrack/" target="_blank">The Inside Track</a>&nbsp;|&nbsp;<a href="http://www.unhmub.com/" target="_blank">Memorial Union Building</a>&nbsp;|&nbsp;<a href="http://www.unh.edu/" target="_blank">UNH</a></p>'); //put message here
 
