77a80c3aa8496cdeb30bb22e945cf2c592c349d3
yllekz <nothingnothingnothin@icloud.com>
2020-03-18 21:06:32 -0400

    Initial file dump / customization
    Initial dump of website files and some rudimentary color selection along with basic nav structure and banner image.

---
+++ beta/javascript/menucontents.js
@@ -0,0 +1,68 @@
+
+//Contents for menu 1
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
+
+
+//Contents for menu 2
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu2.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
+
+
+//Contents for menu 3
+var anylinkmenu3={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Third menu variable. Same precaution.
+anylinkmenu3.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
+
+//Contents for menu 4
+var anylinkmenu4={divclass:'anylinkmenu', inlinestyle:'', linktarget:''}
+anylinkmenu4.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
+
+//Contents for menu 5
+var anylinkmenu5={divclass:'anylinkmenu', inlinestyle:'', linktarget:''}
+anylinkmenu5.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
+
+//Contents for menu 6
+var anylinkmenu6={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //Second menu variable. Same precaution.
+anylinkmenu6.items=[
+	["Item 1", "#"],
+	["Item 2", "#"],
+	["Item 3", "#"],
+	["Item 4", "#"],
+	["Item 5", "#"],
+	["Item 6", "#"] //no comma following last entry!
+]
